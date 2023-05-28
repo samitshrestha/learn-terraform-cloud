ad3b3e2cb952adf5b5061ec0d74e39c83b29b68a
SammitShrestha <sammitman_shrestha1@vfc.com>
2023-05-28 15:46:07 +0545

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "SammitBeyondID"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
